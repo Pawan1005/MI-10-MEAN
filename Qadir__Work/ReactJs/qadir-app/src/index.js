01d441e5db5de31f49808490a29a0cfcc961fe1f
MKadeer17 <m.kadeer2013@gmail.com>
2021-05-20 13:21:14 +0530

    Qadir ReactJS Timer Assignment Done

--- Qadir__Work/ReactJs/qadir-app/src/index.js
+++ Qadir__Work/ReactJs/qadir-app/src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TimerClass from './Components/TimerClass';
+import Timer from './Components/Timer';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+setInterval(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+      <TimerClass />
+      <Timer date={new Date()} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}, 1000);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
